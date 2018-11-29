380816aa96138c2b7bdaf7615e672ac76e7700eb
Avery Vann <averyvann@Averys-MacBook-Pro.local>
2018-11-29 14:08:16 -0500

    fixed enviorment problem

--- src/helpers/enviorment.js
+++ src/helpers/enviorment.js
@@ -7,9 +7,9 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
     // this is the deployed react application
-    case 'ddt-discgolfclient.herokuapp.com':
+    case 'paulhellofront.herokuapp.com':
         // this is the full url of your deployed API
-        APIURL = 'https://ddt-discgolfapi.herokuapp.com'
+        APIURL = 'https://paulhelloback.herokuapp.com'
 }
 
 export default APIURL;
